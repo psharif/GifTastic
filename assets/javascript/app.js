73939ff07917984540793f9d062968095528841c
psharif <psharif@asu.edu>
2018-01-30 10:58:26 -0800

    Added some finishing touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,22 @@
 
-var buttons =[]; 
- 
+/// Starter Buttons That have the subject of Shapes
+var buttons =["Lines", "Triangles", "Circles", "Squares", "Rectangles", "Ovals", "Graphs", "Oblongs", "Octogon", "Hexagon" ]; 
+
+function createButtons(){
+
+	///Empties the Button Area so that the buttons don't make duplicates. 
+	$("#button-area").empty();
+
+	/// For each button in the buttons array. Create a button add a class and append to button area. 
+	buttons.forEach(function(el){
+		var newButton = $("<button class='ui inverted teal button'>"); 
+		newButton.addClass("gif-click");
+		newButton.text(el);
+		$("#button-area").append(newButton);
+	});
+}
+
+/// Gets A Gif based on what buttonText is sent to this function, using an ajax call. 
 function getGifs(buttonText){
 	
 	var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=dc6zaTOxFJmzC&limit=10";  
@@ -14,39 +30,48 @@ function getGifs(buttonText){
 	  //only put up new gifs. 
 	  $("#gifs").empty();
 
-	  ///Creates an img tag for each gif and loads them into the gif area. 
-      response.data.forEach(function(el){
 
-      $("#gifs").append('<div class="column"><div class="img-container"><img src=' + el.images.original_still.url + ' class="gif"></div></div>');
-
-      	/*var newImg = $("<img>");
-      	newImg.attr("src", el.images.original_still.url);
-      	//newImg.attr("data-url", el.images.original_still.url);
-      	newImg.addClass("gif");
-      	$("#gifs").append(newImg);*/
-      });	  
+	  if(response.data.length > 0){
+		  ///Creates an img tag for each gif and a rating and loads them into the gif area. 
+	      response.data.forEach(function(el){
+	      ///Appends Rating and Image to div tag. 
+	      	$("#gifs").append('<div class="column"><p> Rating: ' + el.rating + '</p><div class="img-container"><img src=' + el.images.original_still.url + ' class="gif"></div></div>');
+	      });	  
+	   }
+	   else{
+	   		///Handle Exception 
+	   		console.log("Your Query Returned Nothing");
+	   }
 
 	});
 }
 
+/// Listens for the user to hit the submit button. Grabs the value from the text input, and creates a button.
 $("#submit-button").on("click", function(event){
 	event.preventDefault();
 	//Grabs the value the user pressed and makes the button
-	var	button_value = $("#input").val();
-	var newButton = $("<button class='ui inverted teal button'>"); 
-	newButton.addClass("gif-click");
-	newButton.text(button_value);
-	//Appends the button to the button area. 
-	$("#button-area").append(newButton);
+	var	button_value = $("#input").val().trim();
+	//Sets the input value to nothing. Clears the text feild. 
+	$("#input").val("");
+	if(button_value == ""){
+		//Handle Exception 
+		console.log("You Need To Type Soemthing")
+	}
+	else {
+		buttons.push(button_value);
+		createButtons(); 
+	}
+
 });
 
+// Gets the text from the button when a user presses a button. Sends it to getGifs function. 
 $("body").on("click", ".gif-click", function(){
 	getGifs($(this).text());  
 }); 
 
 ///Listens for Clicks on Gifs 
-///If the gif is still remove _s from the src url. Add a class of playing for the conditional.
 ///If the gif is playing add _s from the src url. Remove the class of playing for the conditional.
+///If the gif is still remove _s from the src url. Add a class of playing for the conditional.
 $("body").on("click", ".gif", function(){
 	if($(this).hasClass("playing")){
 		console.log("Gonna Stop Playing");
@@ -58,4 +83,7 @@ $("body").on("click", ".gif", function(){
 		$(this).attr("src", $(this).attr("src").replace(/\_s\.gif/, ".gif"));
 		$(this).addClass("playing");
 	}
-})
+});
+
+///Initializes App with Initial Buttons
+createButtons();
