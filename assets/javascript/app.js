47f83d639139dd636fef3bf2702c49ab879307a4
psharif <psharif@asu.edu>
2018-01-29 23:10:19 -0800

    Made UI changes, need to fix a few minor things

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,11 +16,14 @@ function getGifs(buttonText){
 
 	  ///Creates an img tag for each gif and loads them into the gif area. 
       response.data.forEach(function(el){
-      	var newImg = $("<img>");
+
+      $("#gifs").append('<div class="column"><div class="img-container"><img src=' + el.images.original_still.url + ' class="gif"></div></div>');
+
+      	/*var newImg = $("<img>");
       	newImg.attr("src", el.images.original_still.url);
       	//newImg.attr("data-url", el.images.original_still.url);
       	newImg.addClass("gif");
-      	$("#gifs").append(newImg);
+      	$("#gifs").append(newImg);*/
       });	  
 
 	});
@@ -30,7 +33,7 @@ $("#submit-button").on("click", function(event){
 	event.preventDefault();
 	//Grabs the value the user pressed and makes the button
 	var	button_value = $("#input").val();
-	var newButton = $("<button>"); 
+	var newButton = $("<button class='ui inverted teal button'>"); 
 	newButton.addClass("gif-click");
 	newButton.text(button_value);
 	//Appends the button to the button area. 
@@ -41,6 +44,9 @@ $("body").on("click", ".gif-click", function(){
 	getGifs($(this).text());  
 }); 
 
+///Listens for Clicks on Gifs 
+///If the gif is still remove _s from the src url. Add a class of playing for the conditional.
+///If the gif is playing add _s from the src url. Remove the class of playing for the conditional.
 $("body").on("click", ".gif", function(){
 	if($(this).hasClass("playing")){
 		console.log("Gonna Stop Playing");
